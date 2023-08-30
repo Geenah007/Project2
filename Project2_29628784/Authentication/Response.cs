5cac676fbd1b45e413143cafb7df902ab27e021a
Tee Goba <29628784@mynwu.ac.za>
2023-08-31 00:35:26 +0200

    Updates commits to main

---
+++ Project2_29628784/Authentication/Response.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JWTAuthentication.Authentication
+{
+    public class Response
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
